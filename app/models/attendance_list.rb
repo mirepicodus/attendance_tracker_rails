cb67f4b31f375544635082a31f747e540514a53d
MIR <mightykiwi@yahoo.com>
2014-08-23 14:45:50 -0700

    add AttendanceList validation for date presence

--- app/models/attendance_list.rb
+++ app/models/attendance_list.rb
@@ -1,3 +1,4 @@
 class AttendanceList < ActiveRecord::Base
   belongs_to :course
+  validates :date, presence: true
 end
--- spec/models/attendance_list_spec.rb
+++ spec/models/attendance_list_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe AttendanceList do
   it{should belong_to :course}
+  it{should validate_presence_of :date}
 end
