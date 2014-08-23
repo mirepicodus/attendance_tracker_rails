8faec16926fd7690ec098ab7c6d11e418053aacd
MIR <mightykiwi@yahoo.com>
2014-08-23 16:04:28 -0700

    add student name validation w/ errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,18 @@
 class StudentsController < ApplicationController
   def list
+    @student = Student.new
     @students = Student.all
     render('students/list.html.erb')
   end
 
   def create
+    @students = Student.all
     @student = Student.new(params[:student])
     if @student.save
       flash[:notice] = "#{@student.name} has been saved."
       redirect_to("/students")
+    else
+      render('students/list.html.erb')
     end
   end
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
   belongs_to :attendance_list
+  validates :name, presence: true
 end
