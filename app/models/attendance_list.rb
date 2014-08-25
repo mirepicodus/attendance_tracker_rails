4cf71c3ec901ba8b59ba3e2f279dbda6d3e90123
MIR <mightykiwi@yahoo.com>
2014-08-24 19:05:22 -0700

    update  message for error uniqueness of attendance_list student to course_day

--- app/models/attendance_list.rb
+++ app/models/attendance_list.rb
@@ -1,5 +1,6 @@
 class AttendanceList < ActiveRecord::Base
   belongs_to :student
   belongs_to :course_day
-  validates :student_id, uniqueness: {scope: :course_day_id}
+  validates :student_id, uniqueness: {scope: :course_day_id, message: "is already used with this date."}
 end
+
