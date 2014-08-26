daf9063749e20bab5b174d339f79d035b381a627
MIR <mightykiwi@yahoo.com>
2014-08-25 20:48:00 -0700

    add course list drop down in course_days

--- app/controllers/course_days_controller.rb
+++ app/controllers/course_days_controller.rb
@@ -1,11 +1,13 @@
 class CourseDaysController < ApplicationController
   def list
+    @courses = Course.all
     @course_day = CourseDay.new
     @course_days = CourseDay.all
     render('course_days/list.html.erb')
   end
 
   def create
+    @courses = Course.all
     @course_days = CourseDay.all
     @course_day = CourseDay.new(params[:course_day])
     if @course_day.save
@@ -17,6 +19,7 @@ class CourseDaysController < ApplicationController
   end
 
   def edit
+    @courses = Course.all
     @course_days = CourseDay.all
     @course_day = CourseDay.find(params[:id])
     if @course_day.update(params[:course_day])
